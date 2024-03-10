2ff437941b80930ad471185923cc30b06839e19b
Leen Odeh <leenodeh287@gmail.com>
2024-03-10 23:36:57 +0300

    add IGenericRepository

---
+++ ATP.DataAccessLayer/IGenericRepo/IGenericRepo.cs
@@ -0,0 +1,10 @@
+﻿namespace ATP.DataAccessLayer.IGenericRepo;
+
+public interface IGenericRepo<T>
+{
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
